97f3bfdb800f5e4adf1c230e55b2892dfc15da40
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-02-28 16:29:16 +0000

    Added Javascript text transformation functions

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 //grab dom elements 
 const textInput = document.getElementById('text');
-const capatalize = document.getElementById('trasnformBtn');
+const capatalize = document.getElementById('transformBtn');
 const output = document.getElementById('transformedInput');
 
-//create an event
-output.addEventListener('click', trasnformText);
+//create event listner
+capatalize.addEventListener('click', transfromText);
 
 //create function to handle the event 
+function transfromText() {
+  const text = textInput.value;
 
-//render output on screen 
+  const transformedText = textTransformer(text);
+
+  output.innerHTML = transformedText;
+}
+
+
+//create function to handle the text transformation.
+function textTransformer(text) {
+  const words = text.toLowerCase().split(' ');
+
+  for (i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+  }
+
+  return words.join(' ');
+};
