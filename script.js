e74382ebb72b1e1e1a0203dc192e02b38d228ef4
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-02-28 16:12:01 +0000

    Grabbed don elements needed. Initialized click event

---
+++ script.js
@@ -0,0 +1,11 @@
+//grab dom elements 
+const textInput = document.getElementById('text');
+const capatalize = document.getElementById('trasnformBtn');
+const output = document.getElementById('transformedInput');
+
+//create an event
+output.addEventListener('click', trasnformText);
+
+//create function to handle the event 
+
+//render output on screen 
